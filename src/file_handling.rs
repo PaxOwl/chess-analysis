aa70dd98ccd8992e86b76c4a6995ee82ab236487
Cyril <cyril.tsilefski@protonmail.com>
2023-09-09 15:22:20 +0200

    Refactoring of the code
        - Splitted the main part into functions
        - Storred file handling functions in a different source file
        - Added a bit of documentation

---
+++ src/file_handling.rs
@@ -0,0 +1,75 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+/// Read the file in argument and store it line by line
+///
+/// # Arguments
+///
+/// `filename` - Name of the file to read
+///
+pub(crate) fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+/// When given an &String element with the right format, isolate the TimeControl sequence
+/// and returns it as an f32
+///
+///
+/// # Arguments
+///
+/// `ip` - A &String holding the sequence to extract
+///
+/// # Examples
+///
+/// ```
+/// Expected format : [TimeControl "60+0"]
+/// In this case, 60. is returned as an f32
+/// ```
+pub(crate) fn get_time_control(line_content: &String) -> i32 {
+    let time: i32;
+    // First instance of " character
+    let start_byte = line_content
+        .find("\"")
+        .unwrap_or(0);
+
+    // First instance of + character
+    let end_byte = line_content
+        .find("+")
+        .unwrap_or(0);
+
+    // Retrieve the TimeControl value as a &str, convert it and store it as an f32 variable
+    let str_time = &line_content[start_byte + 1..end_byte];
+    time = str_time.parse().unwrap();
+
+    return time
+}
+
+/// Given an &String containing the information of a game, extract the total number of moves
+///
+/// # Arguments
+///
+/// `game_moves` - &String containing all the moves in the played game
+///
+pub(crate) fn extract_number_of_moves(game_moves: &String) -> i32 {
+    let mut moves: i32 = 0;
+
+    // Split the string at each space and store it in a collection
+    let collection = game_moves.split(" ").collect::<Vec<&str>>();
+
+    // Iterates backwards and continues until it finds a string that ends with a dot
+    // Since number of moves are the only value ending with dots,
+    // it is a convenient way to filter them
+    for element in collection.into_iter().rev() {
+        if element.chars().last().unwrap() == '.' {
+            // Remove all dots at the end of the string and store it as an i32
+            let element = element.trim_end_matches(".");
+            moves += element.parse::<i32>().unwrap();
+            break;
+        }
+    }
+
+    return moves;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,57 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+mod file_handling;
+
 use std::collections::HashMap;
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+fn hashmap_to_sorted_vector(hash: &HashMap<i32, Vec<i32>>) -> Vec<(i32, Vec<i32>)> {
+    // Convert the keys of the HashMap to a Vector
+    let mut hash_vec:Vec<i32> = Vec::new();
+    for (&key, _) in hash {
+        hash_vec.push(key);
+    }
 
-/// When given an &String element with the right format, isolate the TimeControl element
-/// and returns it as an f32
-///
-///
-/// # Arguments
-///
-/// `ip` - A &String holding the value to extract
-///
-/// # Examples
-///
-/// ```
-/// Expected format : [TimeControl "60+0"]
-/// In this case, 60. is returned
-/// ```
-fn get_time_control(ip: &String) -> f32 {
-        let time: f32;
-    // First instance of " character
-    let start_byte = ip
-        .find("\"")
-        .unwrap_or(0);
+    // Sort the keys Vector
+    hash_vec.sort();
 
-    // First instance of + character
-    let end_byte = ip
-        .find("+")
-        .unwrap_or(0);
+    // Create a sorted Vector and assing the Vectors of values back to their key respective keys
+    let mut sorted_vec = Vec::new();
+    for index in &hash_vec {
+        sorted_vec.push((*index, hash[index].clone()));
+    }
 
-    // Retrieve the TimeControl value as a &str, convert it and store it as an f32 variable
-    let str_time = &ip[start_byte + 1..end_byte];
-    time = str_time.parse().unwrap();
-
-    return time
+    return sorted_vec
 }
 
 fn main() {
-    let mut game_timers = HashMap::new();
-    let mut total_moves: f32 = 0.;
-    let mut lines_counter : f32 = 0.;
-    let mut time: f32 = 0.;
-    if let Ok(lines) = read_lines(
-        "./res/lichess_db_standard_rated_2016-02.pgn"
-        // "./res/temp.pgn"
+    let mut game_timers:HashMap<i32, Vec<i32>> = HashMap::new();
+    let mut time: i32 = 0;
+    if let Ok(lines) = file_handling::read_lines(
+        // "./res/lichess_db_standard_rated_2016-02.pgn"
+        "./res/reduced_data.pgn"
     ) {
         for line in lines {
-            if let Ok(ip) = line {
-                if ip.contains("TimeControl") {
-                    if ip.contains("-") {
-                        time = 0.;
+            if let Ok(line_content) = line {
+                if line_content.contains("TimeControl") {
+                    if line_content.contains("-") {
+                        time = 0;
                     }
                     else {
-                        time = get_time_control(&ip);
+                        time = file_handling::get_time_control(&line_content);
                     }
                 }
-                if ip.starts_with('1') {
-                    lines_counter += 1.0;
-                    println!("Game {}", lines_counter);
-                    let parts = ip.split(" ");
-                    let collection = parts.collect::<Vec<&str>>();
-                    for element in collection.into_iter().rev() {
-                        if element.chars().last().unwrap() == '.' {
-                            let element = element.trim_end_matches(".");
-                            total_moves += element.parse::<i32>().unwrap() as f32;
-                            break;
-                        }
+                if line_content.starts_with('1') {
+                    let moves: i32 = file_handling::extract_number_of_moves(&line_content);
+                    if !game_timers.contains_key(&time) {
+                        game_timers.insert(time, Vec::new());
                     }
-                    if !game_timers.contains_key(&time.to_string()) {
-                        game_timers.insert(time.to_string(), Vec::new());
-                    }
-                    game_timers.get_mut(&time.to_string()).expect("REASON").push(total_moves)
+                    game_timers.get_mut(&time).expect("REASON").push(moves)
                 }
             }
         }
     }
-    for (htime, _) in &game_timers {
-        println!("{}", htime);
+
+    let time_moves:Vec<(i32, Vec<i32>)> = hashmap_to_sorted_vector(&game_timers);
+
+    for (sorted_time, sorted_move) in &time_moves {
+        println!("{} - {}", sorted_time, sorted_move.len());
     }
 }
