6022ac75a8cf5dca48157e31697c2fe8fcfabf01
Cyril Tsilefski <cyril.tsilefski@protonmail.com>
2023-11-11 12:57:09 +0100

    Removes pub from Statistics elements

--- src/statistics.rs
+++ src/statistics.rs
@@ -2,24 +2,24 @@ use crate::game;
 
 #[derive(Clone)]
 pub struct Statistics {
-    pub avg: f32,
-    pub median: i32,
-    pub var: f32,
-    pub std: f32,
-    pub min: i32,
-    pub max: i32,
+    avg: f32,
+    median: i32,
+    var: f32,
+    std: f32,
+    min: i32,
+    max: i32,
 }
 
 impl Statistics {
 
-    pub fn init(games: Vec<game::Game>) -> Statistics {
+    pub fn new(games: &Vec<game::Game>) -> Self {
         let mut avg: f32 = 0.;
         let mut var: f32 = 0.;
         let mut min: i32 = 10000;
         let mut max: i32 = 0;
         let mut moves_list: Vec<i32> = Vec::new();
 
-        for game in &games {
+        for game in games {
             let moves: i32 = game.get_number_of_moves();
             avg += moves as f32;
             if max < moves {
@@ -31,7 +31,7 @@ impl Statistics {
             moves_list.push(moves);
         }
         avg /= games.len() as f32;
-        for game in &games {
+        for game in games {
             var += (game.get_number_of_moves() as f32 - avg).powf(2.);
         }
         var /= games.len() as f32;
